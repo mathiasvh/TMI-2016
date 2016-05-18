149625b0c2e9391b848a8e0e5b3c639a69703955
Mathias <Mathias@Y580-Mathias>
2016-05-18 14:56:52 +0200

    No need of extra mains.

--- src/src/StdOut.java
+++ src/src/StdOut.java
@@ -302,17 +302,4 @@ public final class StdOut {
         out.printf(locale, format, args);
         out.flush();
     }
-
-   /**
-     * Unit tests some of the methods in <tt>StdOut</tt>.
-     */
-    public static void main(String[] args) {
-
-        // write to stdout
-        StdOut.println("Test");
-        StdOut.println(17);
-        StdOut.println(true);
-        StdOut.printf("%.6f\n", 1.0/7.0);
-    }
-
 }
